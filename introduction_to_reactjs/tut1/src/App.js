08e76c276b2343332cd223de42f47153ef200505
Temidola <ayoolatemidola@gmail.com>
2022-02-14 21:25:12 -0500

    add react

--- introduction_to_reactjs/tut1/src/App.js
+++ introduction_to_reactjs/tut1/src/App.js
@@ -2,12 +2,20 @@ const App = () => {
   const now = new Date()
   const a = 10
   const b = 20
+  const c = 50
+  var name = "haryomalificient419"
 
   return(
     <div>
       <p>Hello world, it is {now.toString()}</p>
       <p>
-        {a} plus {b} is {a + b}
+        {a} + {b} is {a + b}
+      </p>
+      <p>
+        {a} + {b} + {c} is equal to {a + b + c}
+      </p>
+      <p>
+        my name is {name}
       </p>
     </div>
   )
