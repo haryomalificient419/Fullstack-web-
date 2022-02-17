5cbbaf679460ac5c0108201de2cba66ded759c7f
Temidola <ayoolatemidola@gmail.com>
2022-02-17 16:55:29 -0500

    add javascript

--- javascript_tut/tut.js
+++ javascript_tut/tut.js
@@ -12,8 +12,14 @@ console.log(x, y) // print 1, 'hello'
 const arr = [1, 2, 3]
 console.log(arr.length)
 console.log(arr[1])
+const arr2 = arr.concat(8)
+const arr3 = arr.map(value => value * 2)
+const arr4 = arr.map(value => '<li>' + value + '</li>')
+const t = [1, 2, 3, 4, 5]
+const [first, second, ...rest] = t
+console.log(arr, arr2, arr3, arr4, first, second, rest)
 
-arr.forEach(value =>{
+arr.forEach(value => {
   arr.push(5)
   console.log(value)
 })
